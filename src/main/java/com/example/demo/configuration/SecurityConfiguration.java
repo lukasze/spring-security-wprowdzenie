81da725ba3dd19e60b851fc04c65a5ef0c58fd7a
lukasze <wyspianski.lukasz@gmail.com>
2022-06-04 18:17:54 +0200

    TODO

--- src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -2,13 +2,7 @@ package com.example.demo.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
@@ -19,28 +13,31 @@ public class SecurityConfiguration {
     @Autowired
     private ObjectMapper objectMapper;
 
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//  HttpSecurity będzie dostępne wraz z zaleznością Spring Security
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // TODO 2b
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .antMatchers("/contact").permitAll()
-                        .anyRequest().authenticated()
-                )
+//        http
+//                .authorizeHttpRequests((authz) -> authz
+//                        .antMatchers("/contact").permitAll()
+//                        .anyRequest().authenticated()
+//                );
         // TODO 3
-                .httpBasic(httpBasic -> {
-                    httpBasic.authenticationEntryPoint((request, response, authException) -> {
+
+//                .httpBasic(httpBasic -> {
+//                    httpBasic.authenticationEntryPoint((request, response, authException) -> {
         // TODO 3a
-                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+//                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         // TODO 3b
-                        response.addHeader("WWW-Authenticate", "Basic");
+
+//                        response.addHeader("WWW-Authenticate", "Basic");
         // TODO 3c
-                        response.getWriter().println(objectMapper.writeValueAsString(Map.of("message", "unauthenticated")));
-                    });
-                });
-        return http.build();
-    }
+
+//                        response.getWriter().println(objectMapper.writeValueAsString(Map.of("message", "unauthenticated")));
+//                });
+//        return http.build();
+//    }
 
 
 
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +11,11 @@ public class IndexController {
     @GetMapping
     public Map<String, Object> index() {
         // TODO 4a zamień wartość mapy na: SecurityContextHolder.getContext().getAuthentication().getName()
-        return Map.of("Welcome",SecurityContextHolder.getContext().getAuthentication().getName());
+        return Map.of("Welcome","to the Jungle!");
     }
         // TODO 2a
-    @GetMapping("/contact")
-    public Map<String, Object> contact() {
-        return Map.of("email", "contact@example.com", "phone", "+48 333 888 222");
-    }
+//    @GetMapping("/contact")
+//    public Map<String, Object> contact() {
+//        return Map.of("email", "contact@example.com", "phone", "+48 333 888 222");
+//    }
 }
